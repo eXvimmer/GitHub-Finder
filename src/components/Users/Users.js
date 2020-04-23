a878aca93a16b5e947a6e7b1734e2012f16e2d67
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-04-23 16:46:46 +0430

    Create Spinner component and add it to Users component.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../Layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
-    const { users, loading } = this.props;
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
         {users.map(user => (
@@ -12,7 +15,12 @@ class Users extends Component {
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
   display: "grid",
